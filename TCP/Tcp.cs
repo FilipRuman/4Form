7710a8637f02252216b698bf41d946020c20a7e7
FR <137614590+FilipRuman@users.noreply.github.com>
2025-05-26 15:02:07 +0200

    added support for sending data through tcp

--- TCP/Tcp.cs
+++ TCP/Tcp.cs
@@ -1,55 +1,61 @@
 namespace ForForm.Tcp
 {
-	using System;
-	using System.Net.Sockets;
-	using System.Text;
-	using System.Threading.Tasks;
-	using Godot;
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Godot;
 
-	public partial class Tcp : Node {
-		[Export]
-		TcpParser tcpParser;
+    public partial class Tcp : Node {
+        [Export]
+        TcpParser tcpParser;
 
-		TcpClient client;
-		NetworkStream stream;
+        TcpClient client;
+        NetworkStream stream;
 
-		private Task readDataTask;
+        private Task readDataTask;
 
-		float tcpConnectionCreationTimer = 0;
-		const float TcpConnectionTimeout = .5f; //S
+        float tcpConnectionCreationTimer = 0;
+        const float TcpConnectionTimeout = .5f; //S
 
-		public override void _Process(double delta) {
-			if (stream == null) {
-				tcpConnectionCreationTimer += (float)delta;
-				if (tcpConnectionCreationTimer > TcpConnectionTimeout) {
-					tcpConnectionCreationTimer = 0;
-					CreateTcpConnection();
-				}
+        public override void _Process(double delta) {
+            if (stream == null) {
+                tcpConnectionCreationTimer += (float)delta;
+                if (tcpConnectionCreationTimer > TcpConnectionTimeout) {
+                    tcpConnectionCreationTimer = 0;
+                    CreateTcpConnection();
+                }
 
-				return;
-			}
-			if (
-				readDataTask == null
-				|| readDataTask.Status == TaskStatus.RanToCompletion
-				|| readDataTask.Status == TaskStatus.Faulted
-			) {
-				readDataTask?.Dispose();
-				readDataTask = ReadDataAsync();
-			}
-			base._Process(delta);
-		}
+                return;
+            }
+            if (
+                readDataTask == null
+                || readDataTask.Status == TaskStatus.RanToCompletion
+                || readDataTask.Status == TaskStatus.Faulted
+            ) {
+                readDataTask?.Dispose();
+                readDataTask = ReadDataAsync();
+            }
+            base._Process(delta);
+        }
 
-		private async Task ReadDataAsync() {
-			byte[] buffer = new byte[1024];
-			int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-			string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-			// GD.Print("Received: " + message);
-			tcpParser.ParseTcpDataString(data);
-		}
+        private async Task ReadDataAsync() {
+            byte[] buffer = new byte[1024];
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            // GD.Print("Received: " + message);
+            tcpParser.ParseTcpDataString(data);
+        }
 
-		private void CreateTcpConnection() {
-			client = new TcpClient("127.0.0.1", 2137);
-			stream = client.GetStream();
-		}
-	}
+        public async Task SendDataAsync(String stringToSend) {
+            var bytesToSend = Encoding.UTF8.GetBytes(stringToSend);
+            await stream.WriteAsync(bytesToSend);
+        }
+
+        private void CreateTcpConnection() {
+            client = new TcpClient("127.0.0.1", 2137);
+            stream = client.GetStream();
+        }
+    }
 }

785f6cb7520e7e69b2fa3a4ed247da392cff923b
FR <137614590+FilipRuman@users.noreply.github.com>
2025-05-26 15:05:11 +0200

    added parsing for new peripherials and connection requests to tcp parser

--- TCP/TcpParser.cs
+++ TCP/TcpParser.cs
@@ -1,10 +1,25 @@
 namespace ForForm.Tcp
 {
+    using System.Text.RegularExpressions;
     using Godot;
 
     public partial class TcpParser : Node {
         [Export]
+        Tcp tcp;
+
+        [Export]
+        Menu.PeripheralsMenu peripheralsMenu;
+
+        [Export]
         Bike.BikeInput bikeInput;
+        RegEx peripheralsNameRegex = new RegEx();
+        RegEx peripheralsIndexRegex = new RegEx();
+
+        public override void _Ready() {
+            peripheralsNameRegex.Compile("|.*|");
+            peripheralsIndexRegex.Compile("[.*]");
+            base._Ready();
+        }
 
         public void ParseTcpDataString(string data) {
             // c# switch statements are UGLY compered to rust...
@@ -20,7 +35,21 @@ namespace ForForm.Tcp
                     uint.TryParse(data[1..data.Length], out bikeInput.currentPower);
                     break;
                 }
+                case 'i':
+                {
+                    var nameRough = peripheralsNameRegex.Search(data[1..data.Length]).Strings[0];
+                    var indexRough = peripheralsIndexRegex.Search(data[1..data.Length]).Strings[0];
+                    peripheralsMenu.HandlePeripheralsConnection(
+                        nameRough[1..(nameRough.Length - 1)],
+                        uint.Parse(indexRough[1..(indexRough.Length - 1)])
+                    );
+                    break;
+                }
             }
+        }
+
+        public void SendPeripheralConnectionRequest(uint index, string deviceTypeName) {
+            tcp.SendDataAsync($"i|{deviceTypeName}|[{index}]");
         }
     }
 }

328fa63ec4e2e581a80bf38509e65f7a43bbb17a
FR <137614590+FilipRuman@users.noreply.github.com>
2025-06-05 20:35:56 +0200

    made bike stats static and moved some stats to bike models
    there will be only one player bike so it is ok

--- godot/Bike/BikeStats.cs
+++ godot/Bike/BikeStats.cs
@@ -2,26 +2,14 @@ namespace ForForm.Bike
 {
     using Godot;
 
-    public partial class BikeStats : Node {
-        [Export]
-        public float bikeMass; //kg
-
-        [Export]
-        public float userMass; //kg
-        public float totalMass => bikeMass + userMass; //kg
-
-        [Export]
-        public float wheelFrictionCoefficient;
-
-        [Export]
-        public float wheelRadius; //m
+    public static class BikeStats {
+        public static BikeModel bikeModel;
+        public static float userMass; //kg
+        public static float totalMass => bikeModel.mass + userMass; //kg
 
         // public float whealCircumference => whealRadius * 2 * Mathf.Pi;
-
-        [Export]
-        public float frontalArea;
-
-        [Export]
-        public float dragCoefficient;
+        public static float drag => dragCoefficient * (frontalArea + bikeModel.frontalArea);
+        public static float dragCoefficient;
+        public static float frontalArea;
     }
 }

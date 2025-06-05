998cebd5da9aa3cddec9d8d86fe48b475920f990
FR <137614590+FilipRuman@users.noreply.github.com>
2025-06-05 20:38:29 +0200

    fixed bug showing speed in m/s rather than in km/h

--- godot/Bike/BikeUIMain.cs
+++ godot/Bike/BikeUIMain.cs
@@ -21,7 +21,7 @@ namespace ForForm.Bike.UI
         public override void _Process(double delta) {
             cadenceLabel.Text = $"cadence  : {bikeInput.currentCadence}";
             slopeLabel.Text = $"slope  : {Math.Round((double)bikePhysics.path.slope, 1)}%";
-            speedLabel.Text = $"speed 󰓅 : {Math.Round(bikePhysics.speed, 1)}km/h";
+            speedLabel.Text = $"speed 󰓅 : {Math.Round(bikePhysics.speedKmH, 1)}km/h";
             powerLabel.Text = $"power 󱐋 : {bikeInput.currentPower}";
             fpsLabel.Text = $"FPS: {Engine.GetFramesPerSecond()}";
             base._Process(delta);

81a9e9d9a5f8c4af23b32c2ec54d8ff06ef6e3cb
FR <137614590+FilipRuman@users.noreply.github.com>
2025-06-19 18:52:35 +0200

    enchanced property edit

--- godot/Menu/PropertyEdit.cs
+++ godot/Menu/PropertyEdit.cs
@@ -11,13 +11,17 @@ namespace ForForm.Menu
         // not used anywhere currently
         float min = 0,
             max = 0;
-        public override void _Ready()
-        {
-            lineEdit.Text = "";
+        bool alreadyDoneTextSetup = false;
+
+        public override void _Ready() {
+            if (!alreadyDoneTextSetup)
+                lineEdit.Text = "";
             base._Ready();
         }
+
         public void SetupStr(string initValue, Action<string> output, bool editable) {
-            lineEdit.Text = initValue.ToString();
+            alreadyDoneTextSetup = true;
+            lineEdit.Text = initValue;
             lineEdit.Editable = editable;
             lineEdit.TextChanged += (t) =>
             {
@@ -26,6 +30,7 @@ namespace ForForm.Menu
         }
 
         public void Setup(float initValue, Action<float> output, bool editable) {
+            alreadyDoneTextSetup = true;
             lineEdit.Text = initValue.ToString();
             lineEdit.Editable = editable;
             lineEdit.TextChanged += (t) =>
@@ -35,7 +40,14 @@ namespace ForForm.Menu
         }
 
         public void ParseAndClamp(string text, Action<float> output) {
-            var value = text.ToFloat();
+            if (
+                !float.TryParse(
+                    text,
+                    System.Globalization.CultureInfo.InvariantCulture,
+                    out float value
+                )
+            )
+                return;
             value = (min == 0 && max == 0) ? value : Mathf.Clamp(value, min, max);
             output(value);
         }

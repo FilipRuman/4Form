7da8769ca8167e70c6e429afd03a079c43ef21ca
FR <137614590+FilipRuman@users.noreply.github.com>
2025-06-19 18:52:17 +0200

    added user config

---
+++ godot/Menu/UserConfigMenu.cs
@@ -0,0 +1,37 @@
+namespace ForForm.Menu
+{
+    using Godot;
+
+    public partial class UserConfigMenu : Control {
+        [Export]
+        PropertyEdit name,
+            mass;
+
+        [Export]
+        Button icon;
+
+        public override void _Ready() {
+GD.Print($"Bike.BikeStats.userConfig.name {Bike.BikeStats.userConfig.name}");
+            name.SetupStr(
+                Bike.BikeStats.userConfig.name,
+                (text) =>
+                {
+                    Bike.BikeStats.userConfig.name = text;
+                    Bike.BikeStats.userConfig.Save();
+                },
+                editable: true
+            );
+            mass.Setup(
+                Bike.BikeStats.userConfig.mass,
+                (num) =>
+                {
+                    Bike.BikeStats.userConfig.mass = num;
+                    Bike.BikeStats.userConfig.Save();
+                },
+                editable: true
+            );
+
+            base._Ready();
+        }
+    }
+}

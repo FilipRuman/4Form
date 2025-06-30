4159fc463d5443075b5f90ff9236802031ded3d1
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2025-06-30 11:51:04 +0200

    removed bike stats + cleaned bike acceleration calculation

--- godot/Bike/BikePhysics.cs
+++ godot/Bike/BikePhysics.cs
@@ -21,42 +21,48 @@ namespace ForForm.Bike
         [Export]
         public float speed_mS; //m/s
         public float speed_kmH => speed_mS * 3.6f; // km/h
-
+        public BikeModel bikeModel;
+        public float userMass_kg;
         [Export]
         float gravity_mS2; //m/s^2
-        float gravity_N => BikeStats.totalMass_kg * gravity_mS2; //N
-        float slopeGravityForce_N => gravity_N * Mathf.Sin(slope_rad); //the force that is pushing you forward from hills
-
-        // If you take a corner that looks like nascar track (curved to the inside) the we would need to account other forces but this doesn't matter
-        float normalGravityForce_N => gravity_N * Mathf.Cos(slope_rad); //the force that is applied directly to the ground
-        float rollingResistance_N =>
-            BikeStats.bikeModel.wheelFrictionCoefficient * normalGravityForce_N; //N
+        public float testingPower = 0;
 
         // I could use formula from my flight sim XD https://github.com/FilipRuman/Flight-sim
         const float StandardAirDensity_kgM3 = 1.2250f; // kg/m^3
-        float airDrag_N =>
-            BikeStats.frontalArea_m
-            * BikeStats.dragCoefficient
-            * StandardAirDensity_kgM3
-            * Mathf.Pow(speed_mS, 2)
-            / 2f; //N
 
-        //https://en.wikipedia.org/wiki/Torque
-        float drivetrainForwardPushing_N => input.currentWatts / Mathf.Max(speed_mS, 1); // N
+        public Map.Route.Route route;
+        public Map.Map map;
+        public float Acceleration_mS2() {
+            float gravity_N = (userMass_kg + bikeModel.mass_kg) * gravity_mS2;
+            float slopeGravityForce_N = gravity_N * Mathf.Sin(slope_rad); //the force that is pushing you forward from hills
 
-        public float testingPower = 0;
+            // If you take a corner that looks like nascar track (curved to the inside) the we would need to account other forces but this doesn't matter
+            float normalGravityForce_N = gravity_N * Mathf.Cos(slope_rad); //the force that is applied directly to the ground
+            float rollingResistance_N = bikeModel.wheelFrictionCoefficient * normalGravityForce_N; //N
 
-        float totalForwardForce_N =>
-            drivetrainForwardPushing_N
-            + testingPower / Mathf.Max(speed_mS, 1)
-            - slopeGravityForce_N
-            - rollingResistance_N
-            - airDrag_N; //N
-        public float acceleration_mS2 => totalForwardForce_N / BikeStats.totalMass_kg; // m/s^2 clamped to remove any weirdness
+            float airDrag_N =
+                bikeModel.frontalArea_m
+                * map.dragCoefficient
+                * StandardAirDensity_kgM3
+                * Mathf.Pow(speed_mS, 2)
+                / 2f; //N
+
+            //https://en.wikipedia.org/wiki/Torque
+            float drivetrainForwardPushing_N = input.currentWatts / Mathf.Max(speed_mS, 1); // N
+
+            float totalForwardForce_N =
+                drivetrainForwardPushing_N
+                + testingPower / Mathf.Max(speed_mS, 1)
+                - slopeGravityForce_N
+                - rollingResistance_N
+                - airDrag_N; //N
+
+            return totalForwardForce_N / (userMass_kg + bikeModel.mass_kg); // m/s^2 clamped to remove any weirdness
+        }
 
         public override void _Process(double delta) {
             // so you don't roll backwards on hills when stopping pedaling
-            speed_mS = Mathf.Max(speed_mS + acceleration_mS2 * (float)delta, .001f);
+            speed_mS = Mathf.Max(speed_mS + Acceleration_mS2() * (float)delta, .001f);
 
             UpdatePath(delta);
             base._Process(delta);
@@ -64,7 +70,7 @@ namespace ForForm.Bike
 
         private void UpdatePath(double delta) {
             float currentProgress_m =
-                Progress + speed_mS * (float)delta * GameSettings.currentMap.speedScale;
+                Progress + speed_mS * (float)delta * map.speedScale;
             CalculateSlope(currentProgress_m);
 
             Progress = currentProgress_m;
@@ -77,19 +83,21 @@ namespace ForForm.Bike
         private void CalculateSlope(float currentProgress_m) {
             Progress = currentProgress_m;
             var backWheal = Position.Y;
-            Progress = currentProgress_m + BikeStats.bikeModel.modelsWheelBase_m;
+            Progress = currentProgress_m + bikeModel.modelsWheelBase_m;
             var frontWheal = Position.Y;
 
             var heightDelta = frontWheal - backWheal;
-            slope_rad = Mathf.Atan(heightDelta / BikeStats.bikeModel.modelsWheelBase_m);
+            slope_rad = Mathf.Atan(heightDelta / bikeModel.modelsWheelBase_m);
             slope_percent = Mathf.Tan(slope_rad) * 100;
         }
 
         public float slope_percent; //%
         public float slope_rad;
 
+
+
         public override void _Ready() {
-            Progress = GameSettings.currentRoute.startingPoint;
+            Progress = route.startingPoint;
             base._Ready();
         }
     }

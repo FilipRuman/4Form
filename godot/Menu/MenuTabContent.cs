8bed0bb54d3f90a508d61dabec8db199ef738ee7
LeaveMyAlpacaAlone <137614590+LeaveMyAlpaca@users.noreply.github.com>
2025-07-02 13:11:04 +0200

    used animations

--- godot/Menu/MenuTabContent.cs
+++ godot/Menu/MenuTabContent.cs
@@ -3,6 +3,9 @@ namespace ForForm.Menu
     using Godot;
 
     public partial class MenuTabContent : Control {
+        [Export]
+        public Animations.UIAnimationPlayer animationPlayer;
+
         [ExportGroup("lock screen settings")]
         [Export]
         public bool gameCantBeStarted;
